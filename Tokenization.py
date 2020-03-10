180462ea1110660c969b5528dce4e1dbad68494b
Zhiyu Huang <34206160+MCZhi@users.noreply.github.com>
2020-03-10 16:59:34 +0800

    Add files via upload

--- Tokenization.py
+++ Tokenization.py
@@ -6,18 +6,17 @@ from CONST import *
 import re
 import itertools
 from File_Reading import getFileContents
-
 def generateTokens(contents, filePath):
 
-	tokens = re.sub(r"[^a-zA-Z0-9\s]|[\x00-\x08\x0b-\x1f\x7f-\xff]+",'',contents)
-	tokens = re.sub(r"\b\w{,1}\b",'', tokens).lower().split()
+    tokens = re.sub(r"^\s+", "", contents).split()
+    print(tokens)
 
-	return list(zip(tokens, itertools.repeat(filePath)))
+    return list(zip(tokens, itertools.repeat(filePath)))
 
 if __name__ == '__main__':
-	# Standalone test
-	filePath = rootDir + "10.txt"
-	contents = getFileContents(filePath)
-	tokens = generateTokens(contents, filePath)
-	print(contents)
-	print(tokens)
+    # Standalone Test
+    filePath = rootDir + "1.txt"
+    contents = getFileContents(filePath)
+    tokens = generateTokens(contents, filePath)
+    print(contents)
+    print(tokens)
