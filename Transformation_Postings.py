a247b729ea38cdda8a611f1b79b6c6460b59b77d
huayshuo@gmail.com <huayshuo@gmail.com>
2020-03-12 20:05:59 +0800

    update

--- Transformation_Postings.py
+++ Transformation_Postings.py
@@ -10,14 +10,21 @@ from File_Reading import GetFileContents
 from Tokenization import GenerateTokens
 from Linguistic_Modules import LingModule
 from Sorting_Tokens import SortTokens
+from collections import OrderedDict
 
 
 def TransformationIntoPostings(sorted_token_pairs):
     # Used dictionary data structure (Hash table)
-    dictionary_ = {}
+    dictionary_ = OrderedDict()
 
-    for a, b in sorted_token_pairs:
-        dictionary_.setdefault(a, []).append(b)
+    previous_key = ''
+    for key, value in sorted_token_pairs:
+        if previous_key != key:
+            previous_key = key
+            dictionary_[previous_key] = [value]
+        else:
+            dictionary_[previous_key].append(value)
+    print(dictionary_)
 
     for key in dictionary_:
         value = dictionary_[key]
@@ -33,9 +40,9 @@ if __name__ == "__main__":
     tokenPair = []
     for i in range(2):
         tokenPair = tokenPair + GenerateTokens(GetFileContents(fileList[i]), fileList[i])
-    
+
     original_list = LingModule(tokenPair)
     sorted_list = SortTokens(original_list)
-   # print(sorted_list)
+    # print(sorted_list)
     transformed_postings = TransformationIntoPostings(sorted_list)
-    print(transformed_postings)
+    # print(transformed_postings)
