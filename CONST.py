273f7784728df073d4a1250347ad4b4c00186523
huayshuo@gmail.com <huayshuo@gmail.com>
2020-03-12 22:19:43 +0800

    v1

--- CONST.py
+++ CONST.py
@@ -1,2 +1,2 @@
 # Root Directory
-rootDir = './Htmp/'
+rootDir = "/home/hshuo/Downloads/6226/CI6226-Assignment-Part0-5/Htmp"
---
+++ Optimization/TrieNode.py
@@ -0,0 +1,70 @@
+# Optimization using Dictionary tree
+
+from Directory_Listing import ListFiles
+from File_Reading import GetFileContents
+from Linguistic_Modules import LingStr
+import time
+from CONST import *
+import sys
+
+
+class TrieNode:
+    def __init__(self):
+        self.nodes = dict()
+        self.is_leaf = False
+        self.docId = []
+
+    def insert(self, word: str, docId: str):
+        curr = self
+        for char in word:
+            if char not in curr.nodes:
+                curr.nodes[char] = TrieNode()
+            curr = curr.nodes[char]
+        curr.is_leaf = True
+        curr.docId.append(docId)
+
+    def insert_many(self, words: [str], docId: str):
+        for word in words:
+            self.insert(word, docId)
+
+    def search(self, word: str):
+        curr = self
+        for char in word:
+            if char not in curr.nodes:
+                return []
+            curr = curr.nodes[char]
+        return curr.docId
+
+
+if __name__ == "__main__":
+    start = time.time()
+
+    # Directory Listing
+    all_files = ListFiles(rootDir)
+    dic_tree = TrieNode()
+
+    for file in all_files:
+        # File Reading
+        file_text = GetFileContents(file)
+        tokens = file_text.split()
+        dic_tree.insert_many([LingStr(item) for item in tokens], file)
+
+    time_index = (time.time() - start) * 1000
+    print("Time for creating index:\t", time_index, "ms")
+    print("Memory for the index:\t", sys.getsizeof(dic_tree), "bytes")
+
+    while True:
+        query = input()
+        q_start = time.time()
+        queries = query.split()
+        queries = [LingStr(item) for item in queries]
+        try:
+            if len(queries) < 2:
+                ff = dic_tree.search(queries[0])
+                print(ff)
+            else:
+                print()
+        except BaseException:
+            print([])
+        q_time = (time.time() - q_start) * 1000
+        print("Time for this query:\t", q_time, "ms")
